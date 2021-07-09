b38b371db525bc2baeb19985595c5e4398355496
xavierms <xavierams08@gmail.com>
2021-07-09 13:39:04 -0400

    Desactivé esta opción y cambie el noImplicitReturns a false para poder hacer que el mecanografiado tolere parámetros sin declarar su tipo

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -5,12 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, campo:string, ...args: any[]): any {
+  transform(value: any, campo:string, args: string): any {
     if(!value)return null;
     if(!args)return value;
 
     return value.filter(singleItem=>
-      singleItem[campo].toLowerCase().includes(args)
+      singleItem[campo].toLowerCase().includes(args.toLowerCase())
       );
   }
 
